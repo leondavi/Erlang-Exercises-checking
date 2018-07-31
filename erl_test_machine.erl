cc76a7203c72dcd6e9303a412a3e4b8add969c8c
david <davidl@robo-team.com>
2018-07-31 17:24:14 +0300

    warning decreasing value was updated

--- erl_test_machine.erl
+++ erl_test_machine.erl
@@ -4,10 +4,11 @@
 -author("David Leon").
 -define(FAILING_GRADE,60).
 -define(FULL_GRADE,100).
+-define(DECREASING_ON_WARNING,0).
 -define(DECREASING_POINTS_VAL,3).%how much points to decrease for each mistake
--define(EXCERCISE_STRING,"ex2").
+-define(EXCERCISE_STRING,"ex5").
 -define(GRADES_FILE_NAME,"grades.txt").
--record(student, {id,module_name,grade,compile_status}).
+-record(student, {id,module_name,grade,compile_status,warning=warning_passed}).
 -export([init/0]).
 
 init()-> 
@@ -17,27 +18,26 @@ init()->
 	ListOfStudentsWithGrades = testing_loop(ListOfStudents,[]),
 	print_results_to_text_file(ListOfStudentsWithGrades).
 
-compile_files([],ListOfStudents)-> ListOfStudents;
+compile_files([],ListOfStudents)-> ListOfStudents; 
 compile_files(FilesList,ListOfStudents)-> SeekID = string:find(hd(FilesList),?EXCERCISE_STRING++"_",leading),
-	case SeekID of
-		nomatch ->  compile_files(tl(FilesList),ListOfStudents);
-		_ ->
-			IDTmp = tl(string:find(hd(FilesList),"_",leading)),
-			ID = hd(lists:filter(fun(X) -> X /= [] end,string:replace(IDTmp,string:find(IDTmp,"."),""))),
-			%io:format("~p~n", [compile:file(hd(FilesList),[return_warnings])]),
-			case compile:file(hd(FilesList),[return_warnings]) of
-				{ok,ModuleName}-> compile_files(tl(FilesList),
-					ListOfStudents++[#student{id=ID,module_name=ModuleName,grade=?FULL_GRADE,compile_status=compilation_passed}]);%compilation failed;
-				{ok,ModuleName,_Warning}-> compile_files(tl(FilesList),
-					ListOfStudents++[#student{id=ID,module_name=ModuleName,grade=?FULL_GRADE-?DECREASING_ON_WARNING,compile_status=compilation_passed,warning=warning_failed}]);%compilation failed;
-				error -> compile_files(tl(FilesList),
-					ListOfStudents++[#student{id=ID,module_name=hd(FilesList),grade=?FAILING_GRADE,compile_status=compilation_failed,warning=warning_failed}])
-			end
+	case SeekID of 
+	nomatch ->  compile_files(tl(FilesList),ListOfStudents);
+	_ ->
+		IDTmp = tl(string:find(hd(FilesList),"_",leading)),
+		ID = hd(lists:filter(fun(X) -> X /= [] end,string:replace(IDTmp,string:find(IDTmp,"."),""))),
+		case compile:file(hd(FilesList),[strong_validation,return_warnings]) of
+			{ok,ModuleName,[]}-> compile_files(tl(FilesList),
+							ListOfStudents++[#student{id=ID,module_name=ModuleName,grade=?FULL_GRADE,compile_status=compilation_passed}]);%compilation failed;
+			{ok,ModuleName,_Warning}-> compile_files(tl(FilesList),
+							ListOfStudents++[#student{id=ID,module_name=ModuleName,grade=?FULL_GRADE-?DECREASING_ON_WARNING,compile_status=compilation_passed,warning=warning_failed}]);%compilation failed;
+			error -> compile_files(tl(FilesList),
+							 ListOfStudents++[#student{id=ID,module_name=hd(FilesList),grade=?FAILING_GRADE,compile_status=compilation_failed}])
+		end
 	end.
 
 print_results_to_text_file([])->done;
 print_results_to_text_file(ListOfStudents)-> Student=hd(ListOfStudents),
-	file:write_file(?GRADES_FILE_NAME,io_lib:fwrite("~s    |   ~p    |    ~p~n",[Student#student.id,Student#student.grade,Student#student.compile_status]),[append]),
+	file:write_file(?GRADES_FILE_NAME,io_lib:fwrite("~s    |   ~p    |    ~p     |    ~p    ~n",[Student#student.id,Student#student.grade,Student#student.compile_status,Student#student.warning]),[append]),
 	print_results_to_text_file(tl(ListOfStudents)).
 
 %filtering file names, keeping .erl files
@@ -51,7 +51,7 @@ testing_loop([],UpdatedList) -> UpdatedList;
 testing_loop(ListOfStudents,UpdatedList) -> CurrentRecord = hd(ListOfStudents),
 	case CurrentRecord#student.compile_status of
 		compilation_failed-> testing_loop(tl(ListOfStudents),UpdatedList++[CurrentRecord]);
-		compilation_passed-> Grade = testing(CurrentRecord#student.module_name),
+		compilation_passed-> Grade = CurrentRecord#student.grade-testing(CurrentRecord#student.module_name),
 				     NewRecord = CurrentRecord#student{grade=Grade},
 				     testing_loop(tl(ListOfStudents),UpdatedList++[NewRecord])
 	end.
